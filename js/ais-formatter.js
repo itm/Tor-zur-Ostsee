48920c2f28f16fce44194e51bf5ace00a683e47e
ebers <ebers@itm.uni-luebeck.de>
2011-12-15 14:36:46 +0100

    function 'createMarkerContent' moved from index.html to js/ais-formatter.js

--- js/ais-formatter.js
+++ js/ais-formatter.js
@@ -25,3 +25,40 @@ var translate = function(ais_description) {
 	}
 	return translation;
 }
+
+var createMarkerContent = function(vesselinfo) {
+
+	var direction = new Array();
+	direction["7"] = "NW";
+	direction["8"] = "N";
+	direction["9"] = "N0";
+	direction["6"] = "0";
+	direction["3"] = "SO";
+	direction["2"] = "S";
+	direction["1"] = "SW";
+	direction["4"] = "W";
+	direction["5"] = "unbekannt";
+	content = 
+       	"<div class='container'>"+
+       	"<img  id='zentriert' border='0' src='http://images.vesseltracker.com/images/vessels/thumbnails/" + vesselinfo.pic +".jpg'/>"+
+       	"<dl class='table-display'>"+
+    	"<dt>Name</dt>"+
+    	"<dd>"+cnvrt2Upper(vesselinfo.name)+"</dd>"+
+    	"<dt>Schiffstyp</dt>"+
+    	"<dd>"+translate(vesselinfo.type)+"</dd>"+
+    	"<dt>Flagge</dt>"+
+    	"<dd>"+ "<img src='http://images.vesseltracker.com/images/flags/"+ vesselinfo.flagid +".png'/> " + vesselinfo.nationality+"</dd>"+
+    	"<dt>L&auml;nge x Breite</dt>"+
+    	"<dd>"+ Math.round(vesselinfo.length) + "m x " + Math.round(vesselinfo.width)+"m</dd>"+
+    	"<dt>Status</dt>"+
+    	"<dd>"+translate(vesselinfo.status)+"</dd>"+
+    	"<dt>Geschwindigkeit</dt>"+
+    	"<dd>"+vesselinfo.speed+"kn</dd>"+
+    	"<dt>Kurs / Richtung</dt>"+
+    	"<dd>"+vesselinfo.course+"&deg; / "+ direction[vesselinfo.icon]+"</dd>"+
+    	"</dl>"+
+    	"</div>"
+
+	console.log(content);
+	return content;
+};
