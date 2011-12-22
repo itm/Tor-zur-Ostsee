332bfb8e3f7a22f5038399d072e9b19246fb5295
Florian <massel@itm.uni-luebeck.de>
2011-12-22 12:21:42 +0100

    added missing break

--- js/ais-formatter.js
+++ js/ais-formatter.js
@@ -56,6 +56,7 @@ var getMarkerImage = function(vessel) {
 			break;
 		case 'coast\_guard\_ships':
 			result += 'coastguardship';
+			break;
 		case 'passenger\_ships':
 			result += 'passengership';
 			break;
