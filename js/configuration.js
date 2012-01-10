14458a726da6f30f9c0058f61f4847e0ae689e6e
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2012-01-10 13:50:23 +0100

    extracted variables to configuration file, closes #31

---
+++ js/configuration.js
@@ -0,0 +1,26 @@
+// path/url to vessel data
+var url = 'xml/data.xml';
+			
+// timout at the very start of the app
+var timerTimeout = 2000;
+
+// time the infoWindow is shown
+var infoTime = 16000;
+
+// time spent on a ship focused until the infowindow pops up
+var pauseTime = 4000;
+
+// time to stay at a viewport if no ships are present
+var noVesselTime = 8000;
+
+// time until the camera/image popup closes
+var camTime = 30000;
+
+// Flags
+var showAutoOnOffButton = true;
+var useCam = false;
+var showShowCam = true;
+
+// Passat area
+var passat_sw = new google.maps.LatLng(53.95871, 10.87989);
+var passat_ne = new google.maps.LatLng(53.95992, 10.88286);
